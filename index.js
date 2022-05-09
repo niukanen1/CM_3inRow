ed5b88edc1ea5643fc32260ee38e614991e9328b
PetrTolstov <57655896+PetrTolstov@users.noreply.github.com>
2022-05-09 21:34:02 +0300

    A

--- index.js
+++ index.js
@@ -31,4 +31,32 @@ function generateNumberSeq(seed) {
     return createMatrix(result) 
 }
 
-generateNumberSeq(58316);
+let workM = generateNumberSeq(58316).slice(0,5);
+console.table(workM)
+
+for (let y = 0; y < workM.length; y++){
+    for (let x = 0; x < workM[y].length; x++){
+        console.log(`Checking for x:${x} y:${y} ${workM[y][x]}`)
+
+        let right = workM[y] != undefined && workM[y][x+2] !=undefined? workM[y][x+2] : "None"
+        let bottom =  workM[y+1] != undefined && workM[y+1][x+1]!= undefined?  workM[y+1][x+1] : "None"
+        let left = workM[y] != undefined && workM[y][x-2]!=undefined? workM[y][x-2] : "None"
+        let top = workM[y-1] != undefined && workM[y-1][x-1]!=undefined? workM[y-1][x-1] : "None"
+        console.log(bottom == workM[y][x])
+        console.log(top == workM[y][x])
+        switch (workM[y][x]){
+            case right:
+                console.log("Match on the right")
+            case bottom:
+                console.log("Match on the bottom")
+            case left:
+                console.log("Match on the left")
+            case top:
+                console.log("Match on the top")
+        }
+
+
+
+    }
+
+    }

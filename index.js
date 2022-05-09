0ca9bb991a01c3448de6db8015ef6c0fed29b875
Ivan Niukanen <ivanniu2003@gmail.com>
2022-05-09 15:52:27 +0300

    createMatrix, intGen

---
+++ index.js
@@ -0,0 +1,34 @@
+// PARAMETERS 
+
+const GAME_FIELD_SIZE = 5; 
+
+
+function createMatrix(list) {
+    const matrix = []; 
+    let subListIndex = 0; 
+    for (let i = 0; i < list.length; i++) {
+        if (i!=0 && i % GAME_FIELD_SIZE == 0) { 
+            subListIndex ++;
+        }
+        if (i == 0 || (i % GAME_FIELD_SIZE) == 0 ) { 
+            matrix[subListIndex] = []
+            matrix[subListIndex].push(list[i]);
+        } else {
+            matrix[subListIndex].push(list[i]);
+        }
+    }
+    console.log(matrix)
+}   
+
+function generateNumberSeq(seed) { 
+    let currentGenNum = seed; 
+    const result = []; 
+
+    for (let i = 0; i < 100; i++) { 
+        currentGenNum = (currentGenNum / seed * 0.99 % 32) * 5000000
+        result.push(Number(String(currentGenNum)[0]))
+    }
+    createMatrix(result) 
+}
+
+generateNumberSeq(58316);
